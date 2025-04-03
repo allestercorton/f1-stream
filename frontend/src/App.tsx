23c4cf3ee4a375a128768a10229fc1af628fc882
Allester Corton <cortonallester@gmail.com>
2025-04-03 22:26:46 +0800

    🪰bugfix(prod): remove react helmet async in the App.tsx to solve the prod issue

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
-import { HelmetProvider } from 'react-helmet-async';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Home from './pages/Home';
 import Signin from './pages/Signin';
@@ -54,12 +53,10 @@ const queryClient = new QueryClient({
 
 export default function App() {
   return (
-    <HelmetProvider>
-      <QueryClientProvider client={queryClient}>
-        <Banner />
-        <RouterProvider router={router} />
-        <Toaster position='top-center' toastOptions={{ duration: 4000 }} />
-      </QueryClientProvider>
-    </HelmetProvider>
+    <QueryClientProvider client={queryClient}>
+      <Banner />
+      <RouterProvider router={router} />
+      <Toaster position='top-center' toastOptions={{ duration: 4000 }} />
+    </QueryClientProvider>
   );
 }
