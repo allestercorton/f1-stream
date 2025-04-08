821b2fad56d24de7a79ea07aa269e989118eb019
Allester Corton <cortonallester@gmail.com>
2025-04-08 16:36:09 +0800

    🐛bugfix(type): fix backend type bug for deployment

--- backend/config/env.ts
+++ backend/config/env.ts
@@ -1,24 +1,33 @@
 import 'dotenv/config';
 import { Env } from '../types/env.types.js';
 
+// Helper function to check if the environment variable exists
+const getEnvVar = (key: string, defaultValue: string = '') => {
+  const value = process.env[key] || defaultValue;
+  if (!value) {
+    throw new Error(`Environment variable ${key} is missing`);
+  }
+  return value;
+};
+
 const env: Env = {
   server: {
     mode: process.env.NODE_ENV || 'development',
     port: Number(process.env.PORT || 5000),
-    url: process.env.SERVER_URL || 'http://localhost:5000',
+    url: getEnvVar('SERVER_URL', 'http://localhost:5000'),
   },
   client: {
-    url: process.env.CLIENT_URL || 'http://localhost:5173',
+    url: getEnvVar('CLIENT_URL', 'http://localhost:5173'),
   },
   mongo: {
-    uri: process.env.MONGO_URI || '',
+    uri: getEnvVar('MONGO_URI'),
   },
   google: {
-    clientID: process.env.GOOGLE_CLIENT_ID || '',
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
+    clientID: getEnvVar('GOOGLE_CLIENT_ID'),
+    clientSecret: getEnvVar('GOOGLE_CLIENT_SECRET'),
   },
   session: {
-    secret: process.env.SESSION_SECRET || '',
+    secret: getEnvVar('SESSION_SECRET'),
     cookie: {
       maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
     },
